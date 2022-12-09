c98bfc434f594f89682cdd2c3c3b9420715258e7
zymelaii <13282135976@sina.cn>
2022-12-09 12:33:52 +0800

    style: restruct project dirs

---
+++ include/lockheresdk/ui/lineinput.h
@@ -0,0 +1,51 @@
+#ifndef LOCKHERE_UI_UTILS_LINEINPUT_H
+#define LOCKHERE_UI_UTILS_LINEINPUT_H
+
+#include <QWidget>
+#include <QPushButton>
+#include <QLabel>
+#include <QLineEdit>
+#include <QLayout>
+#include <QPalette>
+
+namespace LockHere {
+namespace Ui {
+namespace utils {
+
+class LineInput : public QWidget {
+	Q_OBJECT
+
+public:
+	explicit LineInput(QWidget* parent = nullptr);
+
+	bool eventFilter(QObject* obj, QEvent* event) Q_DECL_OVERRIDE;
+	void paintEvent(QPaintEvent* e) Q_DECL_OVERRIDE;
+	void focusInEvent(QFocusEvent* e) Q_DECL_OVERRIDE;
+	void focusOutEvent(QFocusEvent* e) Q_DECL_OVERRIDE;
+
+	void setHint(const QString& hint);
+
+protected slots:
+	void onActivate();
+	void onDeactivate();
+	void onFocusNext();
+
+signals:
+	void activate();
+	void deactivate();
+
+private:
+	QPalette pal_;
+
+	QPushButton* btOption;
+	QLabel*		 lbHint;
+	QLineEdit*	 leInput;
+	QHBoxLayout* hloMain;
+	QVBoxLayout* vloEdit;
+};
+
+}	// namespace utils
+}	// namespace Ui
+}	// namespace LockHere
+
+#endif	 // LOCKHERE_UI_UTILS_LINEINPUT_H
